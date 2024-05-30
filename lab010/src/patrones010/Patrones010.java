b8de63dfb82ca950d5cd6fa10aec8637f6e74ac0
Jeancarlo Morales <72767265+jeancdevx@users.noreply.github.com>
2024-05-30 12:43:53 -0500

    Add files via upload

---
+++ lab010/src/patrones010/Patrones010.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patrones010;
+
+/**
+ *
+ * @author sistemas
+ */
+public class Patrones010 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+
+}
